043383b7502a3b3984f1bd6338d91d767188ef8c
Chris Metivier <cmmetivi@uncg.edu>
2021-06-17 17:32:13 -0400

    eslint support added; DevServer through Webpack added (like Browsersync); sass support added - no separate css file yet;

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,43 @@
 const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   mode: "development",
-  entry: './index.jsx',
+  entry:  path.resolve(__dirname, './src/index.jsx'),
   // This object contains the configuration for Webpack
-  output: {
-    // outputs the bundled file to the dist directory
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
   module: {
     rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        // use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'sass-loader'],
+      },
       {
         // string regex that matches all javascript files in the project directory
         test: /\.(js|jsx)$/,
         // exclude the node_modules directory
         exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            // this object is equivalent to the babel.config.json file
-            "presets": [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ],
-            "comments": false
-          }
-        }
+        use: ['babel-loader', 'eslint-loader']
       }
     ]
+  },
+  // plugins: [new MiniCssExtractPlugin()],
+  output: {
+    // outputs the bundled file to the dist directory
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    // hot: true,
+    historyApiFallback: {
+      index: 'index.html'
+    },
   }
 }
+
+// "npm webpack" to run
+// or "npm run start" to watch
