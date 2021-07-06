f5e84d97c8f2fb8130d7a82a7be7e646ce7bd812
Chris Metivier <cmmetivi@uncg.edu>
2021-07-06 15:34:13 -0400

    refactored base app component into components directory

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  // Return the App component.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Page has been open for <code>count</code> seconds.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,9 @@
 import './styles/style.scss'
-import * as React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/app'
 
 // this is the HTML element in which we want React to render
 const reactDomContainer = document.getElementById('app')
-
-// Babel will transpile this into ES5 syntax
-const App = () => (
-  <>
-    <h1>React.js</h1>
-    <h2>This app has been set up from scratch!</h2>
-  </>
-)
 
 const rootComponent = App();
 // instruct ReactDom to render or React App in the specified HTML element
