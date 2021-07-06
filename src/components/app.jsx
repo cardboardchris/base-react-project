e8575023852f52e2cfc348b051591f0718f92727
Chris Metivier <cmmetivi@uncg.edu>
2021-07-06 15:37:50 -0400

    text changes in app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,12 +3,9 @@ import React from 'react';
 function App() {
   // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Page has been open for <code>count</code> seconds.
-        </p>
-      </header>
+    <div>
+      <h1>Header</h1>
+      <p>This app has been set up from scratch!</p>
     </div>
   );
 }
